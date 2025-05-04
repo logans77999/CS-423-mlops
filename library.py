10da0f754008521fc3caf78c909bdafdffcbae99
Logan Sommerville <125546571+logans77999@users.noreply.github.com>
2025-05-03 17:53:35 -0700

    Update library.py

--- library.py
+++ library.py
@@ -658,24 +658,27 @@ def find_random_state(
 
 
 titanic_transformer = Pipeline(steps=[
-    ('gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
-    ('class', CustomMappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
-    ('joined', CustomOHETransformer('Joined')),
-    ('fare_tukey', CustomTukeyTransformer(target_column='Fare', fence='outer')),
-    ('age_tukey', CustomTukeyTransformer(target_column='Age', fence='inner')),  # you might need this too!
-    ('fare_robust', CustomRobustTransformer('Fare')),
-    ('age_robust', CustomRobustTransformer('Age')),
+    ('map_gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('map_class', CustomMappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
+    ('target_joined', CustomTargetTransformer(col='Joined', smoothing=10)),
+    ('tukey_age', CustomTukeyTransformer(target_column='Age', fence='outer')),
+    ('tukey_fare', CustomTukeyTransformer(target_column='Fare', fence='outer')),
+    ('scale_age', CustomRobustTransformer(target_column='Age')),
+    ('scale_fare', CustomRobustTransformer(target_column='Fare')),
+    ('impute', CustomKNNTransformer(n_neighbors=5)),
     ('passthrough', FunctionTransformer(validate=False)),  #does nothing but does remove warning
-], verbose=True)
+    ], verbose=True)
 
+    ('passthrough', FunctionTransformer(validate=False)),  #does nothing but does remove warning
 customer_transformer = Pipeline(steps=[
-    ('gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
-    ('experience', CustomMappingTransformer('Experience Level', {'low': 0, 'medium': 1, 'high': 2})),
-    ('os_ohe', CustomMappingTransformer('OS', {'Android' : 0, 'iOS' : 1})),
-    ('isp_ohe', CustomOHETransformer('ISP')),
-    ('tukey_age', CustomTukeyTransformer('Age', 'inner')),
-    ('tukey_time spent', CustomTukeyTransformer('Time Spent', 'inner')),
-    ('robust_age', CustomRobustTransformer('Age')),
-    ('robust_time spent', CustomRobustTransformer('Time Spent')),
+    ('map_os', CustomMappingTransformer('OS', {'Android': 0, 'iOS': 1})),
+    ('target_isp', CustomTargetTransformer(col='ISP')),
+    ('map_level', CustomMappingTransformer('Experience Level', {'low': 0, 'medium': 1, 'high':2})),
+    ('map_gender', CustomMappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('tukey_age', CustomTukeyTransformer('Age', 'inner')),  #from chapter 4
+    ('tukey_time spent', CustomTukeyTransformer('Time Spent', 'inner')),  #from chapter 4
+    ('scale_age', CustomRobustTransformer(target_column='Age')), #from 5
+    ('scale_time spent', CustomRobustTransformer(target_column='Time Spent')), #from 5
+    ('impute', CustomKNNTransformer(n_neighbors=5)),
     ('passthrough', FunctionTransformer(validate=False)),  #does nothing but does remove warning
     ], verbose=True)
