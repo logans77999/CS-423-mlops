e2ed78ada691a499369b6a25674048cf277a8387
Logan Sommerville <125546571+logans77999@users.noreply.github.com>
2025-05-04 17:24:22 -0700

    Update library.py

--- library.py
+++ library.py
@@ -6,6 +6,7 @@ from typing import Dict, Any, Optional, Union, List, Set, Hashable, Literal, Tup
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.impute import KNNImputer
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import FunctionTransformer
 import sklearn
 sklearn.set_config(transform_output="pandas")  #says pass pandas tables through pipeline instead of numpy matrices
