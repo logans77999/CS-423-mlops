0875fab7d49aecf20cdf53cf45f6318fc1510329
Logan Sommerville <125546571+logans77999@users.noreply.github.com>
2025-05-10 18:00:11 -0700

    Update library.py

--- library.py
+++ library.py
@@ -688,3 +688,26 @@ customer_transformer = Pipeline(steps=[
     ('impute', CustomKNNTransformer(n_neighbors=5)),
     ('passthrough', FunctionTransformer(validate=False)),  #does nothing but does remove warning
     ], verbose=True)
+
+def dataset_setup(original_table, label_column_name:str, the_transformer, rs, ts=.2):
+  #your code below
+  features = original_table.drop(columns=label_column_name)
+  labels = original_table[label_column_name]
+  X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=ts, shuffle=True,
+                                                    random_state=rs, stratify=labels)  
+
+  X_train_transformed = the_transformer.fit_transform(X_train, y_train)  
+  X_test_transformed = the_transformer.transform(X_test)  
+
+  x_train_numpy = X_train_transformed.to_numpy()  
+  x_test_numpy = X_test_transformed.to_numpy()  
+  y_train_numpy = y_train.to_numpy()
+  y_test_numpy = y_test.to_numpy()  
+
+  return x_train_numpy, x_test_numpy, y_train_numpy, y_test_numpy
+
+def titanic_setup(titanic_table, transformer=titanic_transformer, rs=titanic_variance_based_split, ts=.2):
+    return dataset_setup(titanic_table, 'Survived', transformer, rs, ts)
+
+def customer_setup(customer_table, transformer=customer_transformer, rs=customer_variance_based_split, ts=.2):
+    return dataset_setup(customer_table, 'Rating', transformer, rs, ts)
